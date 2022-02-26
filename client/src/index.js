b2f9aed5aba48865e1aa22d77e194aa046038715
Jeffrey Deng <jeffreytheflash123@gmail.com>
2022-02-26 14:39:13 -0600

    chakra

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+
 import reportWebVitals from './reportWebVitals';
 
+const colors = {
+  brand: {
+    900: '#1a365d',
+    800: '#153e75',
+    700: '#2a69ac',
+  },
+}
+
+const theme = extendTheme({ colors })
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    
+      <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
